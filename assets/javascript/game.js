f90a896a6738c9b3c90a74b01f1e1c480142beaa
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-02-27 19:01:18 -0500

    first code

---
+++ assets/javascript/game.js
@@ -0,0 +1,65 @@
+var userGuess = [];
+var win = 0
+var loss = 0
+var word1 = "albatross";
+var word2 = "eagle";
+var word3 = "birdie";
+var word4 = "par";
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var userGuessWrong = ();
+
+document.onKeyUp = function(event) {
+
+    // Determines which key was pressed.
+
+    var userGuess = event.key;
+
+        }
+
+
+
+function    {
+   word1 = "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"
+
+
+};
+    if(userGuess === a, word1.length = 0, "a")
+        else(userGuessWrong);
+    if(userGuess === l, word1.length = 1, "l")
+        else(userGuessWrong);
+    if(userGuess === b, word1.length = 2, "b")
+        else(userGuessWrong);
+    if(userGuess === a, word1.length = 3, "a")
+        else(userGuessWrong);
+    if(userGuess === t; word1.length = 4, "t")
+        else(userGuessWrong);
+    if(userGuess === r, word1.length = 5, "r")
+        else(userGuessWrong);
+    if(userGuess === o; word1.length = 6, "o")
+        else(userGuessWrong);
+    if(userGuess === s, word1.length = 7 && word1.length = 8, "s")
+
+
+
+
+
+
+
+
+document.getElementById("hangman").innerHTML =` 
+      <p>You chose: </p>
+      <p>Wins: </p>
+      <p>Losses: </p>
+      
+      
+if (userGuess === ){
+        
+        } else if (userGuess === "r" && computerGuess === "s"){
+          
+         
+          } else if (userGuess === "s" && computerGuess === "p") {
+            
+           
+
+            } else if(userGuess === "p" && computerGuess === "s"){
+             
